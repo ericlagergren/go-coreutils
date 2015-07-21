7fab132c129616ef4a13f509c4102570c62be5a8
Robert Deusser <iamthemuffinman@outlook.com>
2015-07-21 13:48:54 -0400

    GNU touch doesn't mess with symlinks by default

--- touch/touch.go
+++ touch/touch.go
@@ -89,7 +89,7 @@ func main() {
 	if len(flag.Args()) > 0 {
 		for i := 0; i < len(flag.Args()); i++ {
 			filename := flag.Arg(i)
-			_, err := os.Stat(filename)
+			_, err := os.Lstat(filename)
 			if err == nil {
 				now := time.Now()
 				os.Chtimes(filename, now, now)
