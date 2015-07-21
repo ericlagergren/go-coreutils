9cfe3ed0f73f752deba3756245c79abc5a230c29
Robert Deusser <iamthemuffinman@outlook.com>
2015-07-21 15:51:04 -0400

    Refactored base64 to conform with go-coreutils

--- base64/base64.go
+++ base64/base64.go
@@ -1,28 +1,73 @@
 /*
+	Go base64 - prints the current working directory.
+	Copyright (C) 2015 Robert Deusser
 	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License version 3 as
-	published by the Free Software Foundation.
-
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+	Written by Robert Deusser <iamthemuffinman@outlook.com>
 */
 
 package main
 
 import (
 	"encoding/base64"
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"unicode"
+
+	flag "github.com/ogier/pflag"
+)
+
+const (
+	Help = `
+Usage: base64 [OPTION]... [FILE]
+Base64 encode or decode FILE, or standard input, to standard output.
+
+Mandatory arguments to long options are mandatory for short options too.
+  -d, --decode          decode data
+  -i, --ignore-garbage  when decoding, ignore non-alphabet characters
+  -w, --wrap=COLS       wrap encoded lines after COLS character (default 76).
+                          Use 0 to disable line wrapping
+
+      --help     display this help and exit
+      --version  output version information and exit
+
+With no FILE, or when FILE is -, read standard input.
+
+The data are encoded as described for the base64 alphabet in RFC 3548.
+When decoding, the input may contain newlines in addition to the bytes of
+the formal base64 alphabet.  Use --ignore-garbage to attempt to recover
+from any other non-alphabet bytes in the encoded stream.
+
+`
+	Version = `
+base64 (Go coreutils) 0.1
+Copyright (C) 2015 Robert Deusser
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+`
+)
+
+var (
+	decode  = flag.BoolP("decode", "d", false, "")
+	ignore  = flag.BoolP("ignore-garbage", "i", false, "")
+	wrap    = flag.IntP("wrap=COLS", "w", 76, "")
+	version = flag.BoolP("version", "v", false, "")
 )
 
 func base64Encode(src []byte) []byte {
@@ -41,11 +86,11 @@ func isAlpha(ch byte) bool {
 	return unicode.IsLetter(rune(ch))
 }
 
-func readAndHandle(reader io.Reader, flagDecode *bool, flagIgnore *bool, flagWrap *int) {
+func readAndHandle(reader io.Reader, decode *bool, ignore *bool, wrap *int) {
 	src, err := readData(reader)
 	checkError(err)
 	var toHandle []byte
-	if *flagIgnore {
+	if *ignore {
 		//It seems that the effect of "base64 -i" in *nix
 		//is not filter the non-alphabet charater.
 		//This flag cannot work as the original *nix command flag.
@@ -57,13 +102,13 @@ func readAndHandle(reader io.Reader, flagDecode *bool, flagIgnore *bool, flagWra
 	} else {
 		toHandle = src
 	}
-	if *flagDecode {
+	if *decode {
 		decoded, err := base64Decode(toHandle)
 		checkError(err)
 		fmt.Printf("%s", string(decoded))
 	} else {
 		encoded := base64Encode(toHandle)
-		wrapPrint(encoded, *flagWrap)
+		wrapPrint(encoded, *wrap)
 	}
 }
 
@@ -97,22 +142,27 @@ func wrapPrint(output []byte, wrap int) {
 }
 
 func main() {
-	flagDecode := flag.Bool("d", false, "Decode the data")
-	flagIgnore := flag.Bool("i", false, "When decoding, ignore non-alphabet characters")
-	flagWrap := flag.Int("w", 76, "Wrap encoded lines after COLS character (default 76). Use 0 to disable line wrapping.")
-
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "%s", Help)
+		os.Exit(1)
+	}
 	flag.Parse()
-	if *flagWrap < 0 {
-		log.Fatalf("invalid wrap size: %d", *flagWrap)
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "%s", Version)
+		os.Exit(0)
+	}
+	if *wrap < 0 {
+		log.Fatalf("invalid wrap size: %d", *wrap)
 	}
 
 	if len(flag.Args()) == 0 {
-		readAndHandle(os.Stdin, flagDecode, flagIgnore, flagWrap)
+		readAndHandle(os.Stdin, decode, ignore, wrap)
 	} else {
 		for i := 0; i < len(flag.Args()); i++ {
 			file, err := os.Open(flag.Args()[i])
 			checkError(err)
-			readAndHandle(file, flagDecode, flagIgnore, flagWrap)
+			readAndHandle(file, decode, ignore, wrap)
 		}
 	}
 }
