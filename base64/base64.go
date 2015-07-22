6d5846ad014748df76739fafc3bc817732a32aaa
Robert Deusser <iamthemuffinman@outlook.com>
2015-07-22 10:59:56 -0400

    Put old license back on base64 and refactored basename

--- base64/base64.go
+++ base64/base64.go
@@ -1,20 +1,15 @@
 /*
-	Go base64 - prints the current working directory.
-	Copyright (C) 2015 Robert Deusser
 	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+	it under the terms of the GNU General Public License version 3 as
+	published by the Free Software Foundation.
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
+
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-	Written by Robert Deusser <iamthemuffinman@outlook.com>
 */
 
 package main
@@ -66,7 +61,7 @@ There is NO WARRANTY, to the extent permitted by law.
 var (
 	decode  = flag.BoolP("decode", "d", false, "")
 	ignore  = flag.BoolP("ignore-garbage", "i", false, "")
-	wrap    = flag.IntP("wrap=COLS", "w", 76, "")
+	wrap    = flag.IntP("wrap", "w", 76, "")
 	version = flag.BoolP("version", "v", false, "")
 )
 
